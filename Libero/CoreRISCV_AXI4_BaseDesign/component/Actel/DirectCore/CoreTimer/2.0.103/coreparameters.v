0074590b355a358f373aa65e9e0a91925114dedb
user <user@ubuntu-14.04.4-64bit-vm>
2017-03-21 10:56:23 +0000

    Updating CoreRISCV_AXI4 for M2S150

--- Libero/CoreRISCV_AXI4_BaseDesign/component/Actel/DirectCore/CoreTimer/2.0.103/coreparameters.v
+++
@@ -1,9 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Wed Nov 16 16:51:55 2016
-// Parameters for CoreTimer
-//--------------------------------------------------------------------
-
-
-parameter FAMILY = 19;
-parameter INTACTIVEH = 1;
-parameter WIDTH = 32;
--- Libero/CoreRISCV_AXI4_BaseDesign/component/work/MSS_SUBSYSTEM_sb_MSS/MSS_SUBSYSTEM_sb_MSS.sdc
+++
@@ -1,6 +0,0 @@
-set_component MSS_SUBSYSTEM_sb_MSS
-# Microsemi Corp.
-# Date: 2016-Jul-30 19:34:15
-#
-
-create_clock -period 48.1928 [ get_pins { MSS_ADLIB_INST/CLK_CONFIG_APB } ]
