3fd131ad69cc4cd0b243f2bbfc6fe43410735525
CLappin <ciaran.lappin@microsemi.com>
2017-06-12 15:23:33 +0100

    Removed Libero folder.

--- Libero/CoreRISCV_AXI4_BaseDesign/component/Actel/DirectCore/COREAHBTOAPB3/3.1.100/coreparameters.v
+++
@@ -1,9 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Mon Mar 13 09:45:19 2017
-// Parameters for COREAHBTOAPB3
-//--------------------------------------------------------------------
-
-
-parameter FAMILY = 19;
-parameter HDL_license = "U";
-parameter testbench = "User";
--- Libero/CoreRISCV_AXI4_BaseDesign/component/Actel/DirectCore/COREJTAGDEBUG/1.0.101/coreparameters.v
+++
@@ -1,8 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Mon Mar 13 09:45:19 2017
-// Parameters for COREJTAGDEBUG
-//--------------------------------------------------------------------
-
-
-parameter FAMILY = 19;
-parameter IR_CODE = 'h55;
