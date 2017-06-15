8e7e8cc796acf9ab5e8e6239cd6e52ab47278b77
CLappin <ciaran.lappin@microsemi.com>
2017-06-15 16:19:49 +0100

    Updating Modify_The_FPGA_Design and Programming_The_Target_Device

--- Modify_The_FPGA_Design/CoreRISCV_AXI4_BaseDesign/component/Actel/DirectCore/COREAHBTOAPB3/3.1.100/coreparameters.v
+++
@@ -1,9 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Mon Jun 12 11:05:42 2017
-// Parameters for COREAHBTOAPB3
-//--------------------------------------------------------------------
-
-
-parameter FAMILY = 19;
-parameter HDL_license = "U";
-parameter testbench = "User";
--- Modify_The_FPGA_Design/CoreRISCV_AXI4_BaseDesign/component/work/MSS_SUBSYSTEM_sb/FABOSC_0/MSS_SUBSYSTEM_sb_FABOSC_0_OSC.sdc
+++
@@ -1,6 +0,0 @@
-set_component MSS_SUBSYSTEM_sb_FABOSC_0_OSC
-# Microsemi Corp.
-# Date: 2017-Jun-12 11:04:35
-#
-
-create_clock -period 20 [ get_pins { I_RCOSC_25_50MHZ/CLKOUT } ]
